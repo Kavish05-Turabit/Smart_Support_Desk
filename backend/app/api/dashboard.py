e2f33069c010cbae73e35b8e0882922bc4276a69
Kavish05-Turabit <kavish.turabit@gmail.com>
2026-01-16 18:32:19 +0530

    feat: Separated stats calc for dashboard and added redis caching

--- backend/app/api/dashboard.py
+++ backend/app/api/dashboard.py
@@ -7,52 +7,13 @@ from app.models.customerModel import Customer
 from app.models.ticketModel import Ticket
 from app.core.session import RedisDependency, DBdependency
 from app.core.auth import AgentDependency
+from app.core.statistics import get_statistics
 
 router = APIRouter()
 
 
 @router.get("/")
 async def show_dashboard(db: DBdependency, redis_client: RedisDependency, user: AgentDependency):
-    cached_data = await redis_client.get("dashboard:all")
-    if cached_data:
-        return json.loads(cached_data)
     
-    tickets = db.query(Ticket).all()
-    tickets_data = [
-        {c.name: getattr(ticket, c.name) for c in ticket.__table__.columns}
-        for ticket in tickets
-    ]
-
-    Customer_count = db.query(Customer).count()
-    Employee_count = db.query(Employee).count()
-    Ticket_count = db.query(Ticket).count()
-
-    stmt = (
-        select(
-            Employee.first_name,
-            # Count tickets where status is 'Open'
-            func.sum(case((Ticket.status == 'Open', 1), else_=0)).label('open_count'),
-            
-            # Count tickets where status is 'Closed'
-            func.sum(case((Ticket.status == 'Closed', 1), else_=0)).label('closed_count'),
-            
-            # Count tickets where status is 'In Progress'
-            func.sum(case((Ticket.status == 'In Progress', 1), else_=0)).label('progress_count')
-        )
-        .join(Ticket, Employee.employee_id == Ticket.assignee_id)
-        .group_by(Employee.employee_id, Employee.first_name)
-    )
-
-    # Execute the query
-    emp_rows = db.execute(stmt).all()
-    emp_details_data = [dict(row._mapping) for row in emp_rows]
-
-    statistics = {
-        "tickets" : tickets_data,
-        "Customer_count": Customer_count,
-        "Employee_count": Employee_count,
-        "Ticket_count": Ticket_count,
-        "Emp_details" : emp_details_data
-    }
-
-    return statistics
+    statistics = get_statistics(user, db, redis_client)
+    return await statistics
---
+++ backend/app/core/statistics.py
@@ -0,0 +1,74 @@
+import json
+from sqlalchemy import select, func, case, cast, Integer
+from sqlalchemy.orm import Session
+from redis import Redis
+
+from app.models.employeeModel import Employee
+from app.models.customerModel import Customer
+from app.models.ticketModel import Ticket
+from app.models.validators import TicketResponse,CustomerResponse,EmployeeResponse
+
+
+async def get_statistics(user, db: Session, redis_client: Redis):
+
+    # tickets , customers , employees , emp_details_data
+
+    dashboard_cache = await redis_client.get("dashboard:admin")
+    if dashboard_cache:
+        stats = json.loads(dashboard_cache)
+        return stats
+
+    try:
+        # tickets
+        ticket_cache = await redis_client.get("tickets:all")
+        if ticket_cache:
+            tickets = json.loads(ticket_cache)
+        else:
+            tickets_data = db.query(Ticket).all()
+            tickets = [TicketResponse.model_validate(t).model_dump(mode='json') for t in tickets_data]
+            await redis_client.set("tickets:all",json.dumps(tickets),ex=60)
+
+        # customers
+        customer_cache = await redis_client.get("customers:all")
+        if customer_cache:
+            customers = json.loads(customer_cache)
+        else:
+            customers_data = db.query(Customer).all()
+            customers = [CustomerResponse.model_validate(c).model_dump(mode='json') for c in customers_data]
+            await redis_client.set("customers:all",json.dumps(customers),ex=60)
+
+        # employees
+        employee_cache = await redis_client.get("employees:all")
+        if employee_cache:
+            employees = json.loads(employee_cache)
+        else:
+            employees_data = db.query(Employee).all()
+            employees = [EmployeeResponse.model_validate(e).model_dump(mode='json') for e in employees_data]
+            await redis_client.set("employees:all",json.dumps(employees),ex=60)
+    except Exception as e:
+        print("Cache Error :- ", e)
+
+    # emp_details_data
+    stmt = (
+        select(
+            Employee.first_name,
+
+            cast(func.sum(case((Ticket.status == 'Open', 1), else_=0)), Integer).label('open_count'),
+            cast(func.sum(case((Ticket.status == 'Closed', 1), else_=0)), Integer).label('closed_count'),
+            cast(func.sum(case((Ticket.status == 'In Progress', 1), else_=0)), Integer).label('progress_count')
+        )
+        .join(Ticket, Employee.employee_id == Ticket.assignee_id)
+        .group_by(Employee.employee_id, Employee.first_name)
+    )
+    emp_rows = db.execute(stmt).all()
+    emp_details_data = [dict(row._mapping) for row in emp_rows]
+
+    stats = {
+        "tickets" : tickets,
+        "customers" : customers,
+        "employees" : employees,
+        "Emp_details" : emp_details_data
+    }
+    await redis_client.set("dashboard:admin",json.dumps(stats),ex=60)
+
+    return stats
